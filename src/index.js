de9dfcbc605b83f752ea4a3853843de20cc58e69
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-21 14:57:37 -0400

    Removed comments and clean up space

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-console.log('%c HI', 'color: firebrick')
-
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = "https://dog.ceo/api/breeds/list/all"
 const dogBreed = document.getElementById("dog-breeds") 
 const dogCard = document.getElementById("dog-image-container")
-// const doggo = document.querySelectorAll("li.doggo") 
-// const dogList = document.getElementsByClassName("doggo") 
+const dogList = document.getElementsByClassName("doggo") 
+const dropDown = document.getElementById("breed-dropdown") 
 
 function getAllDogPics() {
 
@@ -47,51 +45,19 @@ function breedRender (breedDetail) {
   dogBreed.appendChild(li)
 }
 
-//filter breeds
-// breedNames = array of data
-//selects value from dropdown === matches all with match
-// returns new array with just matching....filterMethod 
-// when change only show breed with that start with that letter
-const dropDown = document.getElementById("breed-dropdown") 
 dropDown.addEventListener("click", () => {
-  
-  // assess click value... dropDown.value
   let letter = dropDown.value;
-  console.log(letter)
-  // assign to const
   
-  const dogList = document.getElementsByClassName("doggo") 
-//html collection
   for(const dogLetter of dogList)
-  // go through array of dog names to those that match clicked value
     if (dogLetter.textContent.startsWith(letter)) {
       dogLetter.style.visibility = "visible";
     } else {
       dogLetter.style.visibility = "hidden"
     }
-  
-//   remove hidden class
-// else 
-//   add hidden class
-  
-  console.log(dogList)  // return only matching... true value
-  // display back on Dom in list
-})
-
-
-
-// function filterDog() {
-//   let letter = dropDown.value;
-  
-// }
-
+});
 
 function initialize() {
-
   getAllDogPics()
   getDogBreed()
-  console.log("test")
-  // filterDog()
 }
-
-document.addEventListener("DOMContentLoaded", initialize)
+initialize()
