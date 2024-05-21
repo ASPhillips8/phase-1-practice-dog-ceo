b0249df52f1d35fef1fe79fd6ab1e42db4a1ca73
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-21 14:50:28 -0400

    Add filter to only display matching dog to letter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 console.log('%c HI', 'color: firebrick')
 
+
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = "https://dog.ceo/api/breeds/list/all"
 const dogBreed = document.getElementById("dog-breeds") 
 const dogCard = document.getElementById("dog-image-container")
-const doggo = document.querySelectorAll("li.doggo") 
+// const doggo = document.querySelectorAll("li.doggo") 
+// const dogList = document.getElementsByClassName("doggo") 
+
 function getAllDogPics() {
 
   fetch(imgUrl)
@@ -20,14 +23,16 @@ function dogRender(dogImage) {
     dogCard.appendChild(img)
 }
 
-
 function getDogBreed () {
 
   fetch(breedUrl)
   .then((response) => response.json())
   .then((breedData) => {
-    const breadNames = Object.keys(breedData.message)
-    breadNames.forEach((breedDetail) => breedRender(breedDetail))
+    const breedNames = Object.keys(breedData.message)
+    console.log("before loop:", breedNames)
+    breedNames.forEach((breedDetail) => breedRender(breedDetail)
+  )
+  console.log("outside of for Each:", breedNames)
   })
 }
 
@@ -42,20 +47,51 @@ function breedRender (breedDetail) {
   dogBreed.appendChild(li)
 }
 
-// doggo.addEventListener("click", function () {
-//   li.style.color = "blue"
-// })
+//filter breeds
+// breedNames = array of data
+//selects value from dropdown === matches all with match
+// returns new array with just matching....filterMethod 
+// when change only show breed with that start with that letter
+const dropDown = document.getElementById("breed-dropdown") 
+dropDown.addEventListener("click", () => {
+  
+  // assess click value... dropDown.value
+  let letter = dropDown.value;
+  console.log(letter)
+  // assign to const
+  
+  const dogList = document.getElementsByClassName("doggo") 
+//html collection
+  for(const dogLetter of dogList)
+  // go through array of dog names to those that match clicked value
+    if (dogLetter.textContent.startsWith(letter)) {
+      dogLetter.style.visibility = "visible";
+    } else {
+      dogLetter.style.visibility = "hidden"
+    }
+  
+//   remove hidden class
+// else 
+//   add hidden class
+  
+  console.log(dogList)  // return only matching... true value
+  // display back on Dom in list
+})
 
-//create function to handle click
-// function handleClick(breadDetail) {
-//   console.log("Selected:" , breadDetail)
+
+
+// function filterDog() {
+//   let letter = dropDown.value;
+  
 // }
 
 
 function initialize() {
+
   getAllDogPics()
   getDogBreed()
   console.log("test")
+  // filterDog()
 }
 
-initialize()
+document.addEventListener("DOMContentLoaded", initialize)
