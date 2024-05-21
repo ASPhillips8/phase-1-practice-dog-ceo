fc474180b0757a3d42201d4e7abba8128dd5021f
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-21 17:01:44 -0400

    Added event listender and remove console logs

--- src/index.js
+++ src/index.js
@@ -1,63 +1,63 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = "https://dog.ceo/api/breeds/list/all"
-const dogBreed = document.getElementById("dog-breeds") 
-const dogCard = document.getElementById("dog-image-container")
-const dogList = document.getElementsByClassName("doggo") 
-const dropDown = document.getElementById("breed-dropdown") 
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const breedUrl = "https://dog.ceo/api/breeds/list/all"
+  const dogBreed = document.getElementById("dog-breeds") 
+  const dogCard = document.getElementById("dog-image-container")
+  const dogList = document.getElementsByClassName("doggo") 
+  const dropDown = document.getElementById("breed-dropdown")
+  let breedNames 
 
-function getAllDogPics() {
+  function getAllDogPics() {
 
-  fetch(imgUrl)
-  .then((response) => response.json())
-  .then((dogData) => dogData.message.forEach(dogImage => dogRender(dogImage)))
-}
+    fetch(imgUrl)
+    .then((response) => response.json())
+    .then((dogData) => dogData.message.forEach(dogImage => dogRender(dogImage)))
+  }
 
-function dogRender(dogImage) { 
+  function dogRender(dogImage) { 
 
-    const img = document.createElement("img")
-    img.src = dogImage
-    img.alt = "Doggie"
-    dogCard.appendChild(img)
-}
+      const img = document.createElement("img")
+      img.src = dogImage
+      img.alt = "Doggie"
+      dogCard.appendChild(img)
+  }
+  function getDogBreed () {
 
-function getDogBreed () {
-
-  fetch(breedUrl)
-  .then((response) => response.json())
-  .then((breedData) => {
-    const breedNames = Object.keys(breedData.message)
-    console.log("before loop:", breedNames)
-    breedNames.forEach((breedDetail) => breedRender(breedDetail)
-  )
-  console.log("outside of for Each:", breedNames)
-  })
-}
-
-function breedRender (breedDetail) {
-
-  const li = document.createElement("li")
-  li.textContent = breedDetail
-  li.classList.add("doggo")
-  li.addEventListener("click", () => {
-    li.style.color = "blue" 
+    fetch(breedUrl)
+    .then((response) => response.json())
+    .then((breedData) => {
+      breedNames = Object.keys(breedData.message)
+      breedNames.forEach((breedDetail) => breedRender(breedDetail)
+    )
     })
-  dogBreed.appendChild(li)
-}
+  }
 
-dropDown.addEventListener("click", () => {
-  let letter = dropDown.value;
-  
-  for(const dogLetter of dogList)
-    if (dogLetter.textContent.startsWith(letter)) {
-      dogLetter.style.display = "block";
-    } else {
-      dogLetter.style.display = "none"
-    }
-});
+  function breedRender (breedDetail) {
 
-function initialize() {
-  getAllDogPics()
-  getDogBreed()
-}
-initialize()
+    const li = document.createElement("li")
+    li.textContent = breedDetail
+    li.classList.add("doggo")
+    li.addEventListener("click", () => {
+      li.style.color = "blue" 
+      })
+    dogBreed.appendChild(li)
+  }
+
+  dropDown.addEventListener("click", () => {
+    let letter = dropDown.value;
+    
+    for(const dogLetter of dogList)
+      if (dogLetter.textContent.startsWith(letter)) {
+        dogLetter.style.display = "block";
+      } else {
+        dogLetter.style.display = "none"
+      }
+  });
+
+  function initialize() {
+    getAllDogPics()
+    getDogBreed()
+  }
+  initialize()
+})
